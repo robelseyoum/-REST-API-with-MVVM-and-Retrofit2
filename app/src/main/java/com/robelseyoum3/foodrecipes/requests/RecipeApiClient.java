58c2888bc73f28795afe1d9414e1086eb6c3ad4d
Robel <robelseyoum3@gmail.com>
2020-05-20 20:02:01 +0100

    Executing a Runnable with an Executor

--- app/src/main/java/com/robelseyoum3/foodrecipes/requests/RecipeApiClient.java
+++ app/src/main/java/com/robelseyoum3/foodrecipes/requests/RecipeApiClient.java
@@ -1,22 +1,31 @@
 package com.robelseyoum3.foodrecipes.requests;
 
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import com.robelseyoum3.foodrecipes.AppExecutors;
 import com.robelseyoum3.foodrecipes.models.Recipe;
-import com.robelseyoum3.foodrecipes.repositories.RecipeRepository;
+import com.robelseyoum3.foodrecipes.requests.response.RecipeSearchResponse;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+
+import retrofit2.Call;
+import retrofit2.Response;
 
 import static com.robelseyoum3.foodrecipes.util.Constants.NETWORK_TIMEOUT;
 
 public class RecipeApiClient {
 
+    private static final String TAG = "RecipeApiClient";
     private static RecipeApiClient instance;
     private MutableLiveData<List<Recipe>> mRecipes;
+    private RetrieveRecipesRunnable mRetrieveRecipesRunnable;
 
     public static RecipeApiClient getInstance() {
         if(instance == null){
@@ -33,14 +42,13 @@ public class RecipeApiClient {
         return mRecipes;
     }
 
-    public void searchRecipesApi(){
-        final Future handler = AppExecutors.getInstance().networkIO().submit(new Runnable() {
-            @Override
-            public void run() {
-//                retrieve data from rest api
-//           mRecipes.postValue();
-            }
-        });
+    public void searchRecipesApi(String query, int pageNumber) {
+        if (mRetrieveRecipesRunnable != null) {
+            mRetrieveRecipesRunnable = null;
+        }
+        mRetrieveRecipesRunnable = new RetrieveRecipesRunnable(query, pageNumber);
+
+        final Future handler = AppExecutors.getInstance().networkIO().submit(mRetrieveRecipesRunnable);
 
         AppExecutors.getInstance().networkIO().schedule(new Runnable() {
             @Override
@@ -50,5 +58,57 @@ public class RecipeApiClient {
             }
         }, NETWORK_TIMEOUT, TimeUnit.MILLISECONDS);
     }
+
+    private class RetrieveRecipesRunnable implements Runnable {
+
+        private String query;
+        private int pageNumber;
+        boolean cancelRequest;
+
+        public RetrieveRecipesRunnable(String query, int pageNumber) {
+            this.query = query;
+            this.pageNumber = pageNumber;
+            cancelRequest = false;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Response response = getRecipes(query, pageNumber).execute();
+                if (cancelRequest) {
+                    return;
+                }
+                if (response.code() == 200) {
+                    List<Recipe> list = new ArrayList<>(((RecipeSearchResponse) response.body()).getRecipes());
+                    if (pageNumber == 1) {
+                        mRecipes.postValue(list);
+                    } else {
+                        List<Recipe> currentRecipes = mRecipes.getValue();
+                        currentRecipes.addAll(list);
+                        mRecipes.postValue(currentRecipes);
+                    }
+                } else {
+                    String error = response.body().toString();
+                    Log.e(TAG, "run: " + error);
+                    mRecipes.postValue(null);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                mRecipes.postValue(null);
+            }
+        }
+
+        private Call<RecipeSearchResponse> getRecipes(String query, int pageNumber) {
+            return ServiceGenerator.getRecipeApi().searchRecipe(
+                    query,
+                    String.valueOf(pageNumber));
+        }
+
+        private void cancelRequest() {
+            Log.d(TAG, "cancelRequest: canceling the search request");
+            cancelRequest = true;
+        }
+    }
+
 
 }

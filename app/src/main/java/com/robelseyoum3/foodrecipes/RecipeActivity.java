5d7012d1aa64f8e5600ef6f10a78c576fbf54414
Robel <robelseyoum3@gmail.com>
2020-05-30 00:01:17 +0100

    Setting Widget Properties

--- app/src/main/java/com/robelseyoum3/foodrecipes/RecipeActivity.java
+++ app/src/main/java/com/robelseyoum3/foodrecipes/RecipeActivity.java
@@ -3,6 +3,7 @@ package com.robelseyoum3.foodrecipes;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -12,6 +13,8 @@ import androidx.appcompat.widget.AppCompatImageView;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.robelseyoum3.foodrecipes.models.Recipe;
 import com.robelseyoum3.foodrecipes.util.Resource;
 import com.robelseyoum3.foodrecipes.viewmodels.RecipeViewModel;
@@ -70,6 +73,7 @@ public class RecipeActivity extends BaseActivity {
                                 Log.e(TAG, "onChanged: status : ERROR, Recipe: " + recipeResource.data.getTitle());
                                 Log.e(TAG, "onChanged: EROROR message: " + recipeResource.message);
                                 showProgressBAr(false);
+                                setRecipeProperties(recipeResource.data);
                                 break;
                             }
                             case SUCCESS: {
@@ -77,6 +81,7 @@ public class RecipeActivity extends BaseActivity {
                                 Log.d(TAG, "onChanged: status: SUCCESS, Recipe: " + recipeResource.data.getTitle());
                                 showParent();
                                 showProgressBAr(false);
+                                setRecipeProperties(recipeResource.data);
                                 break;
                             }
                         }
@@ -85,5 +90,49 @@ public class RecipeActivity extends BaseActivity {
             }
         });
     }
+
+    private void setRecipeProperties(Recipe recipe) {
+        if (recipe != null) {
+            RequestOptions options = new RequestOptions()
+                    .placeholder(R.drawable.white_background)
+                    .error(R.drawable.white_background);
+
+            Glide.with(this)
+                    .setDefaultRequestOptions(options)
+                    .load(recipe.getImage_url())
+                    .into(mRecipeImage);
+
+            mRecipeTitle.setText(recipe.getTitle());
+            mRecipeRank.setText(String.valueOf(Math.round(recipe.getSocial_rank())));
+            setIngredients(recipe);
+        }
+    }
+
+    private void setIngredients(Recipe recipe) {
+        mRecipeIngredientsContainer.removeAllViews();
+        if (recipe.getIngredients() != null) {
+            for (String ingredient : recipe.getIngredients()) {
+                TextView textView = new TextView(this);
+                textView.setText(ingredient);
+                textView.setTextSize(15);
+                textView.setLayoutParams(
+                        new LinearLayout.LayoutParams(
+                                ViewGroup.LayoutParams.WRAP_CONTENT,
+                                ViewGroup.LayoutParams.WRAP_CONTENT));
+                mRecipeIngredientsContainer.addView(textView);
+            }
+        } else {
+            TextView textView = new TextView(this);
+            textView.setText("Error retrieving ingredients.\nCheck network connection");
+            textView.setTextSize(15);
+            textView.setLayoutParams(
+                    new LinearLayout.LayoutParams(
+                            ViewGroup.LayoutParams.WRAP_CONTENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT));
+            mRecipeIngredientsContainer.addView(textView);
+        }
+    }
+
+
 
 }
